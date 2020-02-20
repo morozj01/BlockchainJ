6485e115ee7626dc13394eebcbbe87bb02a2bdd8
Justin Moroz <morozj01@nyu.edu>
2020-02-20 10:11:13 -0500

    Debugging confirmation functions

--- src/lib/blockchain/burn-and-confirm.js
+++ src/lib/blockchain/burn-and-confirm.js
@@ -8,7 +8,11 @@ module.exports = function(options){
     return new Promise((resolve,reject)=>{
         send(options).then(function check(value){
             confirm({transaction_hash:value.data[0].transaction_hash}).then(value=>{
-                resolve(value); 
+                if(value.data[0].status == "Declined"){
+                    reject(value);
+                }else{
+                    resolve(value);
+                }
             }).catch(err =>{
                 if(err.error.message == "[There are no data]"){
                     const timeout = Math.ceil(Math.random() * (300 - 1) + 1);
--- src/lib/request.js
+++ src/lib/request.js
@@ -12,17 +12,18 @@ module.exports = function myRequest(options,retryCount){
         }).then((value)=>{
             resolve(value);
             }).catch(err=>{
+                //console.log(err.error)
                 if(err.statusCode == 404) return reject(err.error);
                 else if(err.statusCode == 401) return reject(new Error("Failed to authenticate. Hash invalid"))
                 else if(err.statusCode >= 500){
-                    if (retryCount < this.maxRetries) {
+                    if (retryCount < this.maxRetries && err.error.message != "[There are no data]") {
                         const timeout =  2^retryCount+Math.ceil(Math.random() * (1000 - 1) + 1);
                         const library = this; 
                         setTimeout(function(){
                             myRequest.call(library,options,retryCount+1)
                         },timeout);
                     }else{
-                        //console.log(err.error)
+                        //console.log("here");
                         return reject({error:err.error, message:new Error("API server error. Please contact and administrator")});
                     }
                 }  
